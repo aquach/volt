6a45b88f86e234739ba35caab7ad70b7b3e22b04
Alex Quach <alexhquach@gmail.com>
2011-06-05 15:21:07 -0700

    Fixing glog to use logtostderr.

--- Test/main.cpp
+++ Test/main.cpp
@@ -1,5 +1,13 @@
 #include "Volt/Core/Vector2.h"
+#include <gflags/gflags.h>
+#include <glog/logging.h>
 
 int main (int argc, char** argv) {
+	google::InitGoogleLogging(argv[0]);
+	google::ParseCommandLineFlags(&argc, &argv, true);
+	FLAGS_logtostderr = true;
+
+	LOG(INFO) << "WASSUP";
+
 	return 0;
 }
