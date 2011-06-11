ae47151ed2cee9fa24e1153dc96af9cecba47ae7
Alex Quach <alexhquach@gmail.com>
2011-06-10 18:05:41 -0700

    Added Transform.

--- Volt/Core/Transform.cpp
+++ Volt/Core/Transform.cpp
@@ -1,9 +1,5 @@
 #include "Transform.h"
-#include "FileNode.h"
-
-#include "MonocleToolkit.h"
-#include <math.h>
-#include <stdio.h>
+#include "Math.h"
 
 namespace Monocle
 {
@@ -24,23 +20,6 @@ namespace Monocle
 		scale = next->scale*percent + prev->scale*(1.0f-percent);
 	}
 
-	void Transform::Save(FileNode *fileNode)
-	{
-		if (position != Vector2::zero)
-			fileNode->Write("position", position);
-		if (rotation != 0)
-			fileNode->Write("rotation", rotation);
-		if (scale != Vector2::one)
-			fileNode->Write("scale", scale);
-	}
-
-	void Transform::Load(FileNode *fileNode)
-	{
-		fileNode->Read("position", position);
-		fileNode->Read("rotation", rotation);
-		fileNode->Read("scale", scale);
-	}
-    
     Vector2 Transform::GetDirectionVector()
     {
         return Vector2(sin(rotation*pi / 180.0),cos(rotation*pi / 180.0)*-1.0);
